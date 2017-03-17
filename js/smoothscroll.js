1e484f5a2fbd5caf4be754a26df4037e2bb04194
amosher <alexmosher03@gmail.com>
2017-03-16 20:38:28 -0500

    Update smoothscroll.js

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,13 +1,12 @@
 /**
  * SmoothScroll
- * This helper script created by DWUser.com.  Copyright 2012 DWUser.com.  
- * Dual-licensed under the GPL and MIT licenses.  
+ * This helper script created by DWUser.com.  Copyright 2012 DWUser.com.
+ * Dual-licensed under the GPL and MIT licenses.
  * All individual scripts remain property of their copyrighters.
  * Date: 10-Sep-2012
  * Version: 1.0.1
  */
-if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
-
+i/**f (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
 /**
  * jQuery.ScrollTo - Easy element scrolling using jQuery.
  * Copyright (c) 2007-2012 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
